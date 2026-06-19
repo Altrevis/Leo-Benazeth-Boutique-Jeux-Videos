465ab0f728ccf12f3e0df4fe0705244b5914df6e
leo <leo.benazeth@gmail.com>
2026-06-19 12:26:43 +0200

    lancement sur linux

---
+++ boutique_jeux_videos/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/leo/Bureau/Projet Ynov/dev mobile/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/leo/Bureau/Projet Ynov/Leo-Benazeth-Boutique-Jeux-Videos/boutique_jeux_videos" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/leo/Bureau/Projet Ynov/dev mobile/flutter"
+  "PROJECT_DIR=/home/leo/Bureau/Projet Ynov/Leo-Benazeth-Boutique-Jeux-Videos/boutique_jeux_videos"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzlhNmM0ODQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzdlMmU5NDc3Mg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/leo/Bureau/Projet Ynov/Leo-Benazeth-Boutique-Jeux-Videos/boutique_jeux_videos/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/leo/Bureau/Projet Ynov/Leo-Benazeth-Boutique-Jeux-Videos/boutique_jeux_videos/lib/main.dart"
+)
